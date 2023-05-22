713679012dc303a916a02266c45135674087b4fa
Saraileiva <00164021@uca.edu.sv>
2023-05-22 14:10:47 -0600

    Capa modelo

--- app/src/main/java/com/example/painttracker/data/DummyData.kt
+++ app/src/main/java/com/example/painttracker/data/DummyData.kt
@@ -1,2 +1,10 @@
 package com.example.painttracker.data
 
+import com.example.painttracker.data.model.PaintModel
+
+val name = "Red"
+val author = "PaintKS"
+
+val paints = mutableListOf(
+    PaintModel(name, author),
+)
---
+++ app/src/main/java/com/example/painttracker/data/model/PaintModel.kt
@@ -0,0 +1,6 @@
+package com.example.painttracker.data.model
+
+data class PaintModel(
+    val name: String,
+    val author: String
+)
